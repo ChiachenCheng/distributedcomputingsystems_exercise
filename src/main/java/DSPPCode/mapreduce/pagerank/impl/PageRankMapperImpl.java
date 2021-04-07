7c9a1bc0c5ebb9a8926260ef179ac0c75197db1e
ChiachenCheng <59192936+ChiachenCheng@users.noreply.github.com>
2021-04-07 15:23:37 +0800

    第一轮map能过，剩下的不行

---
+++ src/main/java/DSPPCode/mapreduce/pagerank/impl/PageRankMapperImpl.java
@@ -0,0 +1,60 @@
+package DSPPCode.mapreduce.pagerank.impl;
+
+import DSPPCode.mapreduce.pagerank.question.PageRankMapper;
+import DSPPCode.mapreduce.pagerank.question.ReducePageRankWritable;
+import DSPPCode.mapreduce.pagerank.question.PageRankRunner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+public class PageRankMapperImpl extends PageRankMapper {
+
+    private Map<String, String> rankTable = new HashMap<>();
+
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        /* 步骤2：编写处理逻辑将[K1,V1]转换为[K2,V2]并输出 */
+        if (rankTable.isEmpty()) {
+            URI uri = context.getCacheFiles()[0];
+            FileSystem fs = FileSystem.get(uri, new Configuration());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fs.open(new Path(uri))));
+            String content;
+            while ((content = reader.readLine()) != null) {
+                String[] datas = content.split(" ");
+                rankTable.put(datas[0], datas[1]);
+            }
+        }
+
+        // 以空格为分隔符切分
+        String[] pageInfo = value.toString().split(" ");
+        // 网页的排名值
+        double pageRank = Double.parseDouble(rankTable.get(pageInfo[0]));
+        // 网页的出站链接数
+        int outLink = pageInfo.length - 1;
+        ReducePageRankWritable writable;
+        writable = new ReducePageRankWritable();
+        // 计算贡献值并保存
+        writable.setData(String.valueOf(pageRank / outLink));
+        // 设置对应标识
+        writable.setTag(ReducePageRankWritable.PR_L);
+        // 对于每一个出站链接，输出贡献值
+        for (int i = 1; i < pageInfo.length; i += 1) {
+            context.write(new Text(pageInfo[i]), writable);
+        }
+        writable = new ReducePageRankWritable();
+        // 保存网页信息并标识
+        writable.setData(value.toString());
+        writable.setTag(ReducePageRankWritable.PAGE_INFO);
+        // 以输入的网页信息的网页名称为key进行输出
+        context.write(new Text(pageInfo[0]), writable);
+    }
+}
+
---
+++ src/main/java/DSPPCode/mapreduce/pagerank/impl/PageRankReducerImpl.java
@@ -0,0 +1,52 @@
+package DSPPCode.mapreduce.pagerank.impl;
+
+import DSPPCode.mapreduce.pagerank.question.PageRankReducer;
+import DSPPCode.mapreduce.pagerank.question.ReducePageRankWritable;
+import DSPPCode.mapreduce.pagerank.question.PageRankRunner;
+import java.io.IOException;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class PageRankReducerImpl extends PageRankReducer {
+
+    // 阻尼系数
+    private static final double D = 0.85;
+
+    @Override
+    public void reduce(Text key, Iterable<ReducePageRankWritable> values, Context context)
+            throws IOException, InterruptedException {
+        /* 步骤2：编写处理逻辑将[K2,V2]转换为[K3,V3]并输出 */
+        String[] pageInfo = null;
+        // 从配置项中读取网页的总数
+        int totalPage = context.getConfiguration().getInt(PageRankRunner.TOTAL_PAGE, 0);
+        // 从配置项中读取当前迭代步数
+        int iteration = context.getConfiguration().getInt(PageRankRunner.ITERATION, 0);
+        double sum = 0;
+        for (ReducePageRankWritable value : values) {
+            String tag = value.getTag();
+            // 如果是贡献值则进行求和，否则以空格为分隔符切分后保存到pageInfo
+            if (tag.equals(ReducePageRankWritable.PR_L)) {
+                sum += Double.parseDouble(value.getData());
+            } else if (tag.equals(ReducePageRankWritable.PAGE_INFO)) {
+                pageInfo = value.getData().split(" ");
+            }
+        }
+        // 根据公式计算排名值
+        double pageRank = (1 - D) / totalPage + D * sum;
+        // 更新网页信息中的排名值
+        pageInfo[1] = String.valueOf(pageRank);
+        // 最后一次迭代输出网页名及排名值，而其余迭代输出网页信息
+        StringBuilder result = new StringBuilder();
+        if (iteration == (PageRankRunner.MAX_ITERATION - 1)) {
+            result.append(pageInfo[0]).append(" ").append(String.format("%.5f", pageRank));
+        } else {
+            for (String data : pageInfo) {
+                result.append(data).append(" ");
+            }
+        }
+//        StringBuilder result = new StringBuilder();
+//        result.append(pageInfo[0]).append(" ").append(String.format("%.5f", pageRank));
+        context.write(new Text(result.toString()), NullWritable.get());
+    }
+}
