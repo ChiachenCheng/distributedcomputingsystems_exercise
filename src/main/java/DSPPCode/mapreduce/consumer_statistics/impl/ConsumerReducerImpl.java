b44ed9b89bd6566475f4ac85209c1ccdbcbdac55
ChiachenCheng <59192936+ChiachenCheng@users.noreply.github.com>
2021-04-11 22:06:16 +0800

    Complete

--- src/main/java/DSPPCode/mapreduce/consumer_statistics/impl/ConsumerReducerImpl.java
+++ src/main/java/DSPPCode/mapreduce/consumer_statistics/impl/ConsumerReducerImpl.java
@@ -10,19 +10,19 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class ConsumerReducerImpl extends ConsumerReducer{
-    HashMap<String, Boolean> Customer = new HashMap<>();
     @Override
     protected void reduce(Text key, Iterable<Consumer> values, Context context) throws IOException, InterruptedException{
+        HashMap<String, Boolean> Customer = new HashMap<>();
         BigInteger people = BigInteger.ZERO;
         BigInteger money = BigInteger.ZERO;
         for (Consumer value:values){
             money = money.add(BigInteger.valueOf(value.getMoney()));
-            if (Customer.containsKey(value.getId()))
-                continue;
-            Customer.putIfAbsent(value.getId(), true);
-            people = people.add(BigInteger.ONE);
+            if (!Customer.containsKey(value.getId()))
+                people = people.add(BigInteger.ONE);
+            Customer.put(value.getId(), true);
         }
         String ans = key.toString() + "\t" + people.toString() + "\t" + money.toString();
+        System.out.println(ans);
         context.write(new Text(ans), NullWritable.get());
     }
 }
