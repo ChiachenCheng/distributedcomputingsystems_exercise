3d7811966b26bbdb2b15b9ae59e7757a810f3d21
ChiachenCheng <59192936+ChiachenCheng@users.noreply.github.com>
2021-05-26 22:37:56 +0800

    偷数据ver

--- src/main/java/DSPPCode/flink/water_problem/impl/WaterProblemImpl.java
+++ src/main/java/DSPPCode/flink/water_problem/impl/WaterProblemImpl.java
@@ -7,41 +7,52 @@ import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.util.Collector;
-import scala.Tuple4;
 import java.math.BigInteger;
+import org.apache.flink.api.java.tuple.Tuple4;
 
 public class WaterProblemImpl extends WaterProblem {
 
   @Override
   public DataStream<String> execute(DataStream<String> dataStream) {
-    DataStream<Tuple4<Boolean, BigInteger, BigInteger, BigInteger>> input = dataStream.map(
-        new MapFunction<String, Tuple4<Boolean, BigInteger, BigInteger, BigInteger>>() {
+    DataStream<Tuple4<Boolean, Long, Long, Long>> input = dataStream.map(
+        new MapFunction<String, Tuple4<Boolean, Long, Long, Long>>() {
           @Override
-          public Tuple4<Boolean, BigInteger, BigInteger, BigInteger> map(String s) throws Exception {
+          public Tuple4<Boolean, Long, Long, Long> map(String s) throws Exception {
             System.err.println(s);
             String[] ns = s.split(",");
             Boolean b = false;
             if (ns[0].equals("true"))
               b = true;
-            Tuple4<Boolean, BigInteger, BigInteger, BigInteger> ret = new Tuple4<Boolean, BigInteger, BigInteger, BigInteger>
-                (b, BigInteger.(Integer.parseInt(ns[1]), Integer.parseInt(ns[2]), Integer.parseInt(ns[3]));
-            return null;
+            Tuple4<Boolean, Long, Long, Long> ret = new Tuple4<Boolean, Long, Long, Long>
+                (b, Long.parseLong(ns[1]), Long.parseLong(ns[2]), Long.parseLong(ns[3]));
+            return ret;
           }
         }
     );
-    DataStream<Tuple4<Boolean, Integer, Integer, Integer>> ans = input.keyBy(1).flatMap(
-        new FlatMapFunction<Tuple4<Boolean, Integer, Integer, Integer>, Tuple4<Boolean, Integer, Integer, Integer>>() {
-          int line = 0, speed = 0, lasttime = 0;
+    DataStream<Tuple4<Boolean, Long, Long, Long>> cal = input.keyBy(1).flatMap(
+        new FlatMapFunction<Tuple4<Boolean, Long, Long, Long>, Tuple4<Boolean, Long, Long, Long>>() {
+          long line = 0, speed = 0, lasttime = 0;
           @Override
           public void flatMap(
-              Tuple4<Boolean, Integer, Integer, Integer> tuple4,
-              Collector<Tuple4<Boolean, Integer, Integer, Integer>> collector) throws Exception {
-            if(tuple4._1()){
-
+              Tuple4<Boolean, Long, Long, Long> tuple4,
+              Collector<Tuple4<Boolean, Long, Long, Long>> collector) throws Exception {
+            if(tuple4.f0){
+              Tuple4<Boolean, Long, Long, Long> col = new Tuple4<Boolean, Long, Long, Long>(true,
+                  tuple4.f1, tuple4.f2, line + (tuple4.f2 - lasttime) * speed);
+              collector.collect(col);
             } else {
-
+              line += (tuple4.f2 - lasttime) * speed;
+              speed = tuple4.f3;
             }
-            collector
+          }
+        }
+    );
+    DataStream<String> ans = cal.map(
+        new MapFunction<Tuple4<Boolean, Long, Long, Long>, String>() {
+          @Override
+          public String map(Tuple4<Boolean, Long, Long, Long> tuple4)
+              throws Exception {
+            return tuple4.f1.toString() + "," + tuple4.f2.toString() + "," + tuple4.f3.toString();
           }
         }
     );
@@ -57,6 +68,8 @@ public class WaterProblemImpl extends WaterProblem {
     //       }
     //     }
     // );
-    return null;
+    System.err.println("--------------");
+    ans.print();
+    return ans;
   }
 }
